c0c5dd404581ddfccf5abafcfdb38e32d326fe6f
xuanhua2318 <thuongxuan.2323@gmail.com>
2025-12-24 11:46:44 +0700

    add meta viewpor ; add alt to <img> ; create the func in .\index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+var randomNumber1 = Math.floor((Math.random()) * 6 + 1);
+if (randomNumber1 == 1) {
+    document.querySelectorAll("img")[0].setAttribute("src","./images/dice1.png");
+}
+else if (randomNumber1 == 2) {
+    document.querySelectorAll("img")[0].setAttribute("src", "./images/dice2.png");
+}
+else if (randomNumber1 == 3) {
+    document.querySelectorAll("img")[0].setAttribute("src", "./images/dice3.png");
+}
+else if (randomNumber1 == 4) {
+    document.querySelectorAll("img")[0].setAttribute("src", "./images/dice4.png");
+}
+else if (randomNumber1 == 5) {
+    document.querySelectorAll("img")[0].setAttribute("src", "./images/dice5.png");
+}
+else if (randomNumber1 == 6) {
+    document.querySelectorAll("img")[0].setAttribute("src", "./images/dice6.png");
+}
+
+var randomNumber2 = Math.floor((Math.random()) * 6 + 1);
+if (randomNumber2 == 1) {
+    document.querySelectorAll("img")[1].setAttribute("src","./images/dice1.png");
+}
+else if (randomNumber2 == 2) {
+    document.querySelectorAll("img")[1].setAttribute("src", "./images/dice2.png");
+}
+else if (randomNumber2 == 3) {
+    document.querySelectorAll("img")[1].setAttribute("src", "./images/dice3.png");
+}
+else if (randomNumber2 == 4) {
+    document.querySelectorAll("img")[1].setAttribute("src", "./images/dice4.png");
+}
+else if (randomNumber2 == 5) {
+    document.querySelectorAll("img")[1].setAttribute("src", "./images/dice5.png");
+}
+else if (randomNumber2 == 6) {
+    document.querySelectorAll("img")[1].setAttribute("src", "./images/dice6.png");
+}
